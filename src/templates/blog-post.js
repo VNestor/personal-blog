bafb1ce64e892ce6b663e936fce956f6014e6347
Victor Nestor <victor.n.x35@gmail.com>
2022-03-09 01:25:27 -0500

    Changed query name.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -67,7 +67,7 @@ const BlogPostTemplate = ({ data, location }) => {
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-  query blogPostBySlug(
+  query BlogPostBySlug(
     $id: String!
     $previousPostId: String
     $nextPostId: String
